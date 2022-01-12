2bd8aed5283eac60925c8c02f4be207ac4799005
Aginath <agi.selliah@hotmail.fr>
2022-01-12 11:57:06 +0000

    Initial commit

---
+++ Nintendo/NintendoProject/src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+import model.Console;
+import model.Jeu;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Console c= new Console("Xbox");
+		Jeu j= new Jeu("Warzone", c);
+
+	}
+
+}
